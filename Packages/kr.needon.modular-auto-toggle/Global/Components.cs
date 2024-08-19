670fe993b5c01e9711951ae6d39c0048681aa34f
j.lee <j-lee@kaijo.co.jp>
2024-08-19 10:14:47 +0900

    Fix : 유니티 에디터에서 재생후 중지할떄 아이콘 재로드시 다른 모듈러아바타 컴포넌트의 아이콘도 교체되어버리는 문제 수정

--- Packages/kr.needon.modular-auto-toggle/Global/Components.cs
+++ Packages/kr.needon.modular-auto-toggle/Global/Components.cs
@@ -3,5 +3,7 @@
     public static class Components
     {
         public const string DEFAULT_COMPONENT_NAME = "Toggles";
+
+        public const string TOGGLE_TOOL_IDENTIFIER = "toggle_tool_modular";
     }
 }
